f8f1868a85949fc372a727bf10335aa892fa9f26
BUHARI SAIDU <207902399+MuhmdBuharisaeed@users.noreply.github.com>
2026-02-10 07:11:47 +0100

    Update settings-ui.js

--- settings-ui.js
+++ settings-ui.js
@@ -27,3 +27,8 @@ document.addEventListener("DOMContentLoaded",()=>{
   }
 
 });
+
+document.getElementById("acceptPoliciesBtn")?.addEventListener("click",()=>{
+  acceptPolicies();
+  alert("Policies accepted successfully.");
+});
