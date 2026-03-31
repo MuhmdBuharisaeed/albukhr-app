bca44e70841044e23b3331c1a2e43a3f12e9b807
BUHARI SAIDU <207902399+MuhmdBuharisaeed@users.noreply.github.com>
2026-03-31 09:26:43 +0100

    Remove CORE_LIQUIDITY_KEY from localStorage

--- js/core-liquidity-engine.js
+++ js/core-liquidity-engine.js
@@ -5,6 +5,7 @@
 
 const CORE_LIQUIDITY_KEY = "albukhr_core_liquidity_initialized";
 
+localStorage.removeItem("albukhr_core_liquidity_initialized");
 /* =========================================
    INIT CORE PROJECT TREASURIES
 ========================================= */
