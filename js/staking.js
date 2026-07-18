263adfd9cdf06071633877f1f1f96134f4916cdf
BUHARI SAIDU <207902399+MuhmdBuharisaeed@users.noreply.github.com>
2026-07-18 14:37:26 +0100

    Replace 'mainnet' with CURRENT_NETWORK variable

--- js/staking.js
+++ js/staking.js
@@ -9,6 +9,13 @@ const SUPABASE_KEY = "sb_publishable_mSbWlhVKdmSjasKJC50QYw_5wzgRMe2";
 const INTERNAL_KEY = "albukhr_stakes";
 
 /* ======================================
+   CURRENT NETWORK
+====================================== */
+const CURRENT_NETWORK =
+location.hostname.startsWith("test.")
+  ? "testnet"
+  : "mainnet";
+/* ======================================
    STORAGE
 ====================================== */
 function _safeParse(key){
@@ -126,7 +133,7 @@ async function createPendingStake({
 
         status: "pending",
 
-        network: "mainnet",
+        network: CURRENT_NETWORK
 
         payment_id: null,
 
@@ -341,7 +348,7 @@ try{
             payment_id: payment.paymentId,
             txid: payment.txid,
             status: "paid",
-            network: "mainnet"
+            network: CURRENT_NETWORK
         }
     );
 
@@ -395,7 +402,7 @@ async function getAllStakesMerged(){
   try{
 
     const res = await fetch(
-      `${SUPABASE_URL}/rest/v1/stakes?select=*&userid=eq.${user.uid}&network=eq.mainnet`,
+      `${SUPABASE_URL}/rest/v1/stakes?select=*&userid=eq.${user.uid}&network=eq.${CURRENT_NETWORK}`,
       {
         headers:{
           "apikey": SUPABASE_KEY,
@@ -435,7 +442,7 @@ async function getGlobalStakes(){
   try{
 
     const res = await fetch(
-`${SUPABASE_URL}/rest/v1/stakes?select=*&network=eq.mainnet`,
+`${SUPABASE_URL}/rest/v1/stakes?select=*&network=eq.${CURRENT_NETWORK}`,
       {
         headers:{
           "apikey": SUPABASE_KEY,
@@ -528,7 +535,7 @@ const user = JSON.parse(
 if(user?.uid){
 
   const res = await fetch(
-  `${SUPABASE_URL}/rest/v1/withdraw_requests?select=*&userid=eq.${user.uid}&project=eq.${project}&type=eq.capital&status=eq.paid&network=eq.mainnet`,
+  `${SUPABASE_URL}/rest/v1/withdraw_requests?select=*&userid=eq.${user.uid}&project=eq.${project}&type=eq.capital&status=eq.paid&network=eq.${CURRENT_NETWORK}`,
   {
     headers:{
       "apikey": SUPABASE_KEY,
@@ -569,7 +576,7 @@ async function getUserStakes(){
   const user = getCurrentUser();
 
   const res = await fetch(
-  `${SUPABASE_URL}/rest/v1/stakes?select=*&userid=eq.${user.uid}&network=eq.mainnet`,
+  `${SUPABASE_URL}/rest/v1/stakes?select=*&userid=eq.${user.uid}&network=eq.${CURRENT_NETWORK}`,
   {
     headers:{
       "apikey": SUPABASE_KEY,
@@ -602,7 +609,7 @@ if(!user?.uid){
   }
 
   const res = await fetch(
-  `${SUPABASE_URL}/rest/v1/stakes?select=*&userid=eq.${user.uid}&network=eq.mainnet`,
+  `${SUPABASE_URL}/rest/v1/stakes?select=*&userid=eq.${user.uid}&network=eq.${CURRENT_NETWORK}`,
   {
     headers:{
       "apikey": SUPABASE_KEY,
@@ -719,7 +726,7 @@ if(!user?.uid){
   }
 
   const res = await fetch(
-    `${SUPABASE_URL}/rest/v1/stakes?project=eq.${project}&userid=eq.${user.uid}&network=eq.mainnet&order=created_at.asc`,
+    `${SUPABASE_URL}/rest/v1/stakes?project=eq.${project}&userid=eq.${user.uid}&network=eq.${CURRENT_NETWORK}&order=created_at.asc`,
     {
       headers:{
         "apikey": SUPABASE_KEY,
